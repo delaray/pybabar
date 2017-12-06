fce20ccb5fb113fed43e60b8ec1e80172ffda970
Pierre de Lacaze <delaray@hotmail.com>
2017-12-06 08:00:43 -0500

    Added get_word_def

--- src/lexicon.py
+++ src/lexicon.py
@@ -7,6 +7,12 @@ import scraper
 # Merriam Webster Scraper
 #--------------------------------------------------------------------
 
+#--------------------------------------------------------------------
+# Word Part Of Speech
+#--------------------------------------------------------------------
+
+# Returns the part of speech from the Merriam Webster responnse page.
+
 def extract_pos (response):
     pos = []
     soup = BeautifulSoup(response.content, 'lxml')
@@ -16,6 +22,8 @@ def extract_pos (response):
     pos = pos[0].contents
     return pos[0]
 
+#--------------------------------------------------------------------
+
 def get_word_pos(word):
     word_url = 'https://www.merriam-webster.com/dictionary/' + word
     response = scraper.get_url_response(word_url)
@@ -23,13 +31,30 @@ def get_word_pos(word):
     return pos
 
 #--------------------------------------------------------------------
+# Word Definition
+#--------------------------------------------------------------------
 
+word_def_class = ['dt', '']
+
+def extract_def (response):
+    pos = []
+    classes = []
+    soup = BeautifulSoup(response.content, 'lxml')
+    for link in soup.find_all('span'):
+        # if not link.get('class')==None:
+        #     classes.append(link.get('class'))
+        if link.get('class')==word_def_class:
+            pos.append(link)
+    #defs = classes[0].contents
+    return pos # defs
+
+#--------------------------------------------------------------------
 
 def get_word_definition(word):
-    print ("Not implemmented")
-    return None
-
-
+    word_url = 'https://www.merriam-webster.com/dictionary/' + word
+    response = scraper.get_url_response(word_url)
+    defn = extract_def(response)
+    return defn
 
 #--------------------------------------------------------------------
 # End of File
