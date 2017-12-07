b2a4c6232ee0d119b407e8d179a273d359c0f82d
Pierre de Lacaze <delaray@hotmail.com>
2017-12-07 09:54:32 -0500

    Added Merriam-Webster scaper and wor2vec

---
+++ src/semantic.py
@@ -0,0 +1,7 @@
+import gensim
+
+data_file =  'c:\\Projects\\Python\\data\\GoogleNews-vectors-negative300.bin'
+
+# Load Google's pre-trained Word2Vec model.
+def load_model(data_file=data_file):
+    model = gensim.models.KeyedVectors.load_word2vec_format(data_file, binary=True)  
