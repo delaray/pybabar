fa588125377063ea28f3a256d76a7b91aa0c2e4e
Pierre de Lacaze <delaray@hotmail.com>
2020-12-31 15:50:43 +0100

    Fixed get_word_definition.

--- src/lexicon.py
+++ src/lexicon.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup, SoupStrainer
 
 
 # Project Imports
-from src.scraper import get_url_response
+from src.scraper import get_url_data
 
 #--------------------------------------------------------------------
 # Merriam Webster Scraper
@@ -45,27 +45,33 @@ def get_word_pos(word):
 
 word_def_class = ['dt', '']
 
-def extract_definitionx(response):
+def extract_definition(html):
     pos = []
     classes = []
-    soup = BeautifulSoup(response.content, 'lxml')
-    results =  soup.find_all('span')
-    print ('Span length: ' + str(len(results)))
-    for link in results:
-        # if not link.get('class')==None:
-        #     classes.append(link.get('class'))
-        if link.get('class')==word_def_class:
-            pos.append(link)
-    #defs = classes[0].contents
-    return pos # defs
+    soup = BeautifulSoup(html)
+    results =  soup.find_all('meta', {'name' : "description"})
+    if len(results) > 0:
+        x = results[0].attrs['content']
+        x = x.split(':')
+        x = x[0].split('-')
+        return x[1].strip()
+    else:
+        return None
 
 #--------------------------------------------------------------------
 
+# Example URL:
+
+MW1 = 'https://www.merriam-webster.com/dictionary/facetious'
+
 def get_word_definition(word):
     word_url = 'https://www.merriam-webster.com/dictionary/' + word
-    response = get_url_response(word_url)
-    definition = extract_definition(response)
-    return definition
+    html = get_url_data(word_url)
+    if html is not None:
+        definition = extract_definition(html)
+        return definition
+    else:
+        return None
 
 #--------------------------------------------------------------------
 # End of File
